a2446ef4407f5fa81f3d79f09bf949b885ea1856
ppwire <poom.mth@gmail.com>
2022-01-16 13:44:34 +0700

    Change Nav, Add Background Image, Adjust Styling

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,7 @@
 export default function Content() {
    return (
      <section>
-       <h1>Fun facts about React</h1>
+       <h1 className="content-header">Fun facts about React</h1>
        <ul className="content-items">
          <li>Was first released in 2013</li>
          <li>Was originally created by Jordan Walke</li>
